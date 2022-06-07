8ae01c81e16a01b028da75ecb296d4ed3398e4f3
Nate J <nathan.apple131@gmail.com>
2022-06-07 16:00:39 -0600

    Basic edits

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Word
+{
+    public void get_word(){
+        string word = "AHHHH";
+
+        Console.WriteLine($"The new word is {word}");
+    }
+    
+
+}
+
+
+public class Director
+{   
+    public static void Main(string[] args){
+        
+        Word new_word = new Word();
+        new_word.get_word();
+    }
+
+}
