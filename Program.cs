c3a4f5c5206b6eb94e9ba4e5b339acc1799c36c6
Nate J <nathan.apple131@gmail.com>
2022-07-06 13:19:02 -0600

    More basic updates

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ public class Word
         string[] words = {"apple", "mother", "love", "attendance", "lab"};
         int word_num = rnd.Next(5);
 
-        string word = words[word_num + 1];
+        string word = words[word_num];
         
         return word;
     }
@@ -27,17 +27,41 @@ public class Word
     }       
 }
 
+public class Player
+{
+    public string get_input(){
+        Console.WriteLine("Please guess a Letter: ");
+        string input = Console.ReadLine();
+
+        return input;
+    }
+
+
+
+}
+
 
 public class Director
 {   
     public static void Main(string[] args){
         
         Word new_word = new Word();
+        Player player = new Player(); 
 
         string game_word = new_word.get_word();
         int word_length = game_word.Length;
         new_word.show_spaces(word_length);
-        
+        char[] letters = game_word.ToCharArray();
+        string input = player.get_input();
+
+        if(game_word.Contains(input)){
+           
+        }
+        else{
+            Console.WriteLine("nope");
+        }
+
+
 
     }
 }
